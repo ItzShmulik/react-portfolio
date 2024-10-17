5c230528d16266171345fce52899a84b74b7e4ae
shmulik55 <gshloman@gmail.com>
2024-10-17 15:26:06 +0300

    Initial Commit - Converted previous version of portfolio website to be react based

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+    return (
+      <>
+        <nav id="navbar">
+            <a href="#hero-section">About me</a>
+            <a>My Projects</a>
+            <a>Contact Me</a>
+        </nav>
+        <div id="hero-section">
+        <script type="module" src="./main.js"></script>
+          <div id="hero-text">
+            <h1 id="hero-title">Hello, I'm Guy</h1>
+            <h2 id="hero-description">Front-End Developer</h2>
+          </div>
+        </div>
+        <div id="projects-section">
+        
+        </div>
+      </>
+    )
+}
+
+export default App
